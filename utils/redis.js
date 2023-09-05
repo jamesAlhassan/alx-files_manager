8d74632702aa2a2616039c03bcaacc15fb658f7b
James Alhassan <jamesalhassan052@gmail.com>
2023-09-05 09:08:56 +0000

    Add redis.js

---
+++ utils/redis.js
@@ -0,0 +1,19 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+/**
+ * Class for performing operations with Redis service
+ */
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+
+    this.client.on('connect', () => {
+      // console.log('Redis client connected to the server');
+    });
+  }
