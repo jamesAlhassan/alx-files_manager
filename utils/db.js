cfe1f32199e5ef89bf1ac934d5363f548f86832b
James Alhassan <jamesalhassan052@gmail.com>
2023-09-05 10:40:55 +0000

    Update db.js -- Add nbUsers()

--- utils/db.js
+++ utils/db.js
@@ -23,12 +23,20 @@ class DBClient {
     });
   }
 
-
-	 /**
+  /**
    * Checks if connection to Redis is Alive
    * @return {boolean} true if connection alive or false if not
    */
   isAlive() {
     return Boolean(this.db);
+  }
+
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 }
