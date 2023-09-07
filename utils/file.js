cc014aad2d6ff38385915bdb833f03a31341a6c0
James Alhassan <jamesalhassan052@gmail.com>
2023-09-07 04:51:31 +0000

    Update file.js

--- utils/file.js
+++ utils/file.js
@@ -138,7 +138,7 @@ const fileUtils = {
     return { error: null, newFile };
   },
 
- /**
+  /**
    * Updates a file document in database
    * @query {obj} query to find document to update
    * @set {obj} object with query info to update in Mongo
@@ -152,7 +152,63 @@ const fileUtils = {
     );
     return fileList;
   },
-};
 
+  /**
+   * Makes a file public or private
+   * @request {request_object} express request obj
+   * @setPublish {boolean} true or false
+   * @return {object} error, status code and updated file
+   */
+  async publishUnpublish(request, setPublish) {
+    const { id: fileId } = request.params;
+
+    if (!basicUtils.isValidId(fileId)) { return { error: 'Unauthorized', code: 401 }; }
+
+    const { userId } = await userUtils.getUserIdAndKey(request);
+
+    if (!basicUtils.isValidId(userId)) { return { error: 'Unauthorized', code: 401 }; }
+
+    const user = await userUtils.getUser({
+      _id: ObjectId(userId),
+    });
+
+    if (!user) return { error: 'Unauthorized', code: 401 };
+
+    const file = await this.getFile({
+      _id: ObjectId(fileId),
+      userId: ObjectId(userId),
+    });
+
+    if (!file) return { error: 'Not found', code: 404 };
+
+    const result = await this.updateFile(
+      {
+        _id: ObjectId(fileId),
+        userId: ObjectId(userId),
+      },
+      { $set: { isPublic: setPublish } },
+    );
+
+    const {
+      _id: id,
+      userId: resultUserId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    } = result.value;
+
+    const updatedFile = {
+      id,
+      userId: resultUserId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    };
+
+    return { error: null, code: 200, updatedFile };
+  },
+};
 
 export default fileUtils;
